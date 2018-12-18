86ffb10ec70dd5343b5dabcfb1e2a5fc9b58979b
drolsen <dolsen@connectivedx.com>
2018-12-18 06:16:15 -0800

    Fixes modifiers example file structure

--- build/scaffolding/modifiers/[name].example.jsx
+++ build/scaffolding/modifiers/[name].example.jsx
@@ -1,7 +1,23 @@
+/*
+  Example:
+    ```
+      examples: [{
+        name: 'Default styling',
+        component: (
+          <div className={{name}}></div>
+        ),
+      },
+    ```
+*/
+
 export default [{
-  name: 'Default styling',
-  component: (
-    <div className="{{name}}">Lorem ipsum</div>
-  ),
-  devNotes: ''
-}];
+  examples: [
+    {
+      name: 'Default styling',
+      component: (
+        <div className="{{name}}">Lorem ipsum</div>
+      ),
+      devNotes: ''
+    }
+  ]
+};
