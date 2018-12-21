e1e82b783a2442fd22e3e6a3922d0f1bc80637e7
drolsen <dolsen@connectivedx.com>
2018-12-21 09:43:56 -0800

    POSTCSS Updates / cleanup

--- build/configs/css/css.postcss-plugins.config.js
+++ build/configs/css/css.postcss-plugins.config.js
@@ -21,7 +21,6 @@ const resolver = ResolverFactory.createResolver({
 // cssnano as your last plugin call is all you need to remember when add new plugins,
 // however always read newly added plugin docs to make sure they don't carry their own order of operation requirements.
 // standard
-const preset = require('postcss-preset-env');
 const imports = require('postcss-import');
 const nested = require('postcss-nested');
 const extend = require('./css.postcss-extend.plugin.js');
@@ -32,7 +31,6 @@ const colors = require('./css.postcss-colors.plugin.js');
 const media = require('./css.postcss-media.plugin.js');
 const mediaPacker = require('css-mqpacker');
 
-
 module.exports = {
   plugins: [
     imports({ // Allows for @import and our entry point for namespace alias config
@@ -40,13 +38,13 @@ module.exports = {
         return resolver.resolveSync({}, basedir, id);
       }
     }),
-    colors(), // Pre-parse color variables
-    variables(),
-    media(),
-    nested(), // Allows for nested selectors
-    extend(), // Allows for CSS @extend
-    mixins(), // Allows for CSS mixins
-    mediaPacker(), // Allows for the consolidation of @media queries 
+    colors(),       // Pre-parse color variables
+    variables(),    // Allows var(--variables)
+    media(),        // Allows for custom media queries
+    nested(),       // Allows for nested selectors
+    extend(),       // Allows for CSS @extend
+    mixins(),       // Allows for CSS mixins
+    mediaPacker(),  // Allows for the consolidation of @media queries 
     minification(), // Minification of our final CSS results.
   ]
 };
