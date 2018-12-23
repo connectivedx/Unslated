78355f6e5df12b10e7e8e10a59cbbc4f8236886a
drolsen <dolsen@connectivedx.com>
2018-12-23 09:24:39 -0800

    Adds updates to colors page and new icons page

--- src/elements/atoms/Icon/IconSet.js
+++ src/elements/atoms/Icon/IconSet.js
@@ -3,28 +3,4 @@ const __svg__ = { // eslint-disable-line
   name: 'assets/svgs/iconset-[hash].svg'
 };
 
-((options) => {
-  let ajax = new XMLHttpRequest();
-
-  if (typeof XDomainRequest !== 'undefined') {
-    ajax = new XDomainRequest();
-  }
-
-  ajax.open('GET', options.filename, true);
-  ajax.onprogress = () => {};
-  ajax.onload = () => {
-    if (!ajax.responseText || ajax.responseText.substr(0, 4) !== '<svg') {
-      throw Error('Invalid SVG Response');
-    }
-    if (ajax.status < 200 || ajax.status >= 300) {
-      return;
-    }
-
-    const div = document.createElement('div');
-    div.innerHTML = ajax.responseText;
-
-    document.body.insertBefore(div, document.body.childNodes[0]);
-  };
-
-  ajax.send();
-})(__svg__);
+require('webpack-svgstore-plugin/src/helpers/svgxhr')(__svg__);
--- src/pages/colors.jsx
+++ src/pages/colors.jsx
@@ -1,144 +1,13 @@
+//
+// See build/guide/guide.utilities.jsx for any GuideUtils methods used below.
+//
 import colors from '!!style-loader!css-loader!@vars/colors';
 import { List, List__item } from '@atoms/List/List';
 import Rhythm from '@atoms/Rhythm/Rhythm';
 import Heading from '@atoms/Heading/Heading';
 
-// Replaces var(--color-name) with value prior to sampling.
-const cleanVariables = (colors) => {
-	Object.keys(colors).map(key => {
-		if (colors[key].indexOf('var') !== -1) {
-			const variable = colors[key].replace('var(', '').replace(')', '');
-			colors[key] = colors[variable];
-		}
-	});
-	return colors;
-};
 
-const getLuminanace = (r, g, b) => {
-    let a = [r, g, b].map(v => {
-        v /= 255;
-        return v <= 0.03928
-            ? v / 12.92
-            : Math.pow( (v + 0.055) / 1.055, 2.4 );
-    });
-
-    return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
-}
-
-const HexToRGB = (color) => {
-  color = color.replace('#', '');
-  if (color.length < 6) {
-    color = color + color;
-  }
-
-  return [
-    parseInt(color.substr(0,2), 16),
-    parseInt(color.substr(2,2), 16),
-    parseInt(color.substr(4,2), 16)
-  ];
-}
-
-const RGBToHex = (color) => {
-  const hexConvert = (rgb) => {
-    let hex = rgb.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
-  };
-
-  return "#" + hexConvert(color[0]) + hexConvert(color[1]) + hexConvert(color[2]);
-};
-
-const getContrast = (controlcolor, testcolor) => {
-  const controlRGB = HexToRGB(controlcolor);
-  const testRGB = HexToRGB(testcolor);
-
-  let result = (getLuminanace(testRGB[0], testRGB[1], testRGB[2]) + 0.05) / (getLuminanace(controlRGB[0], controlRGB[1], controlRGB[2]) + 0.05);
-  if (result < 1) result = 1 / result;
-  return result;
-}
-
-// Builds hex, and rgb color units for testing purposes
-
-const getColorUnits = (color) => {
-  const chromaObj = {
-    hex: '',
-    rgb: { r: '', g: '', b: '' },
-    hsl: { h: '', s: '', l: '' }
-  };
-
-  const isHex = (color.indexOf('#') !== -1);
-  const isRGB = (color.indexOf('rgb') !== -1);
-  const isHLS = (color.indexOf('hls') !== -1);
-
-  /* if color is a simple hex */
-  if (isHex || !isHex && !isRGB && !isHLS) {
-  	// applies color as a hex to chromaObj.hex
-    chromaObj.hex = color;
-
-    // converts hex into rgb and applies to chromaObj.rgb
-    [...chromaObj.rgb] = HexToRGB(color);
-
-    // converts hex into hls and applies to chromaObj.hls
-    //[...chromaObj.hsl] = chroma(color).hsl();
-
-  /* if color is rbg(a) */
-  } else if (isRGB) {
-  	// rgb object
-    const rgb = color.split('(')[1].split(')')[0].split(',');
-
-    // rgb parts variables
-    const red = parseInt(rgb[0], 10);
-    const green = parseInt(rgb[1], 10);
-    const blue = parseInt(rgb[2], 10);
-
-    // applies rgb(a) to chromaObj.rgb
-    [...chromaObj.rgb] = rgb;
-
-    // converts rgb into hex, and applies hex to chromaObj.hex (with or without alpha)
-    chromaObj.hex = RGBToHex([red, green, blue]);
-
-    // capture HSL colors as fallback
-    //[...chromaObj.hsl] = chroma(chromaObj.hex).hsl();
-  }
-
-  return chromaObj;
-};
-
-// Flags colors based on WCAG ratio, size and level specs
-const WCAGTest = (ratio, size, level) => {
-  switch (level) {
-    case 'A':
-      return 'PASS';
-    case 'AA':
-      if (size === 'large' && ratio > 3) {
-        return 'PASS';
-      } else if (size === 'large--bold' && ratio > 3) {
-        return 'PASS';
-      } else if (size === 'normal' && ratio > 4.5) {
-        return 'PASS';
-      }
-      return 'FAIL';
-
-    case 'AAA':
-      if (size === 'large' && ratio > 4.5) {
-        return 'PASS';
-      } else if (size === 'large--bold' && ratio > 4.5) {
-        return 'PASS';
-      } else if (size === 'normal' && ratio > 7) {
-        return 'PASS';
-      }
-      return 'FAIL';
-
-    default:
-      if (size === 'large' && ratio > 3) {
-        return 'PASS';
-      } else if (size === 'normal' && ratio > 4.5) {
-        return 'PASS';
-      }
-      return 'FAIL';
-  }
-};
-
-// AccessibilityLevel element performs our tests methods above
+// AccessibilityLevel
 const AccessibilityLevel = (props) => {
   const {
     tagName: Tag,
@@ -165,7 +34,7 @@ const AccessibilityLevel = (props) => {
   const createBadges = (type) => {
 		return Object.keys(weights).map((key, value) => {
 			return (<div key={key} className={`AccessibilityLevel__badge AccessibilityLevel__badge--${type} ${((weights[key] !== 'normal' || level !== 'AA')? 'hide' : '')} AccessibilityLevel__badge--${weights[key]}`}>
-	      {WCAGTest((type === 'secondary') ? contrastSecondary : contrastPrimary, weights[key].split('--')[0], level)}
+	      {GuideUtils.WCAGTest((type === 'secondary') ? contrastSecondary : contrastPrimary, weights[key].split('--')[0], level)}
 	    </div>);
 		});
   };
@@ -199,24 +68,24 @@ AccessibilityLevel.propTypes = {
 };
 
 
-const cards = Object.keys(cleanVariables(colors)).map(key => {
+const cards = Object.keys(GuideUtils.cleanColorVariables(colors)).map(key => {
 	if (colors[key] === 'true') { return; }
   //if (key.match('alpha')) { return; }
 
-	const colorUnits = getColorUnits(colors[key]);
+	const colorUnits = GuideUtils.getColorUnits(colors[key]);
 
 	if (!colorUnits.hex) { return; }
 
 	return <List__item key={key}>
 		<div>
       <AccessibilityLevel 
-      	contrastPrimary={getContrast(colors['--color-text--primary'], colorUnits.hex)} 
-      	contrastSecondary={getContrast(colors['--color-text--secondary'], colorUnits.hex)} 
+      	contrastPrimary={GuideUtils.getColorContrast(colors['--color-text--primary'], colorUnits.hex)} 
+      	contrastSecondary={GuideUtils.getColorContrast(colors['--color-text--secondary'], colorUnits.hex)} 
       	level="AA" 
       />
       <AccessibilityLevel 
-      	contrastPrimary={getContrast(colors['--color-text--primary'], colorUnits.hex)}
-      	contrastSecondary={getContrast(colors['--color-text--secondary'], colorUnits.hex)}
+      	contrastPrimary={GuideUtils.getColorContrast(colors['--color-text--primary'], colorUnits.hex)}
+      	contrastSecondary={GuideUtils.getColorContrast(colors['--color-text--secondary'], colorUnits.hex)}
       	level="AAA"
       />
 
