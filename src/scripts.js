04ff8536e56f3aef753a8e714004853ad359f43f
drolsen <dolsen@connectivedx.com>
2018-12-26 11:27:30 -0800

    Adding modal molecule

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,7 @@
 import '@atoms/Icon/IconSet'; // see ./src/elements/atoms/Icon/IconSet.js
 import Form from '@molecules/Form/Form.Container';
 import Tabs from '@molecules/Tabs/Tabs.Container';
+import Modal from '@molecules/Modal/Modal.Container';
 import Expand from '@molecules/Expand/Expand.Container';
 import Accordion from '@molecules/Accordion/Accordion.Container';
 
@@ -9,4 +10,5 @@ setTimeout(() => {
 	Utils.initComponent('Expand', '.expand', Expand);
 	Utils.initComponent('Accordion', '.accordion', Accordion);
 	Utils.initComponent('Tabs', '.tabs', Tabs);
+	Utils.initComponent('Modal', '.modal', Modal);
 }, 1000);
