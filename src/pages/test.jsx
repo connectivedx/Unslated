3f0c8498c94108e546e99719a2dd6e60f6f37665
drolsen <dolsen@connectivedx.com>
2019-01-21 07:41:45 -0800

    Multiple fixes since pulling in eslint

--- src/pages/test.jsx
+++
@@ -1,9 +0,0 @@
-import Accordion from '@molecules/Accordion/Accordion.example';
-
-const page = () => (
-  <React.Fragment>
-    {Accordion[0].examples[0].component}
-  </React.Fragment>
-);
-
-export default page;
