75113ea7be112953df581965244b7b0706f297c7
drolsen <dolsen@connectivedx.com>
2019-04-25 15:30:10 -0700

    Fixing SVG plugin for Unix systems

--- build/configs/img/img.svg.plugin.js
+++ build/configs/img/img.svg.plugin.js
@@ -32,15 +32,18 @@ class WebpackSvgSpritely {
 
       // Grab a collection of icons based on filters option
       Object.keys(compilation.modules).map(j => {
-        const resource = compilation.modules[j].resource;
-        if (!resource) { return; }
-        if (resource.indexOf('Icon\\assets\\') !== -1 && resource.indexOf('.svg') !== -1) {
+        const path = compilation.modules[j].resource;
+        if (!path) { return; }
+        let name = path.replace(/\\/g, '/');
+        if (name.indexOf('Icon/assets') !== -1 && name.indexOf('.svg') !== -1) {
+          name = name.split('/')[name.split('/').length - 1];
+          name = name.split('.')[0];
           this.icons.push({
-            name: resource.split('\\')[resource.split('\\').length - 1].split('.')[0],
-            path: resource
-          }); // get filename off file path.
+            name,
+            path
+          });
         }
-      });
+      }).filter((n) => n);
 
       // Use found icons above to collect svg into symbols
       Object.keys(compilation.assets).map(i => {
@@ -52,16 +55,16 @@ class WebpackSvgSpritely {
           }
 
           // Development builds runs with ._source
-          if (compilation.assets[i]._source) {           
+          if (compilation.assets[i]._source) {
             Object.keys(compilation.assets[i]._source.children).map(j => {
               let child = compilation.assets[i]._source.children[j];
               if (typeof child !== 'string') {
                 if (!child._value) { return; }
-                child._value = child._value.replace(/iconset-(.*)\.svg/g, this.newHash);        
+                child._value = child._value.replace(/iconset-(.*)\.svg/g, this.newHash);
               } else {
-                child = child.replace(/iconset-(.*)\.svg/g, this.newHash);                            
+                child = child.replace(/iconset-(.*)\.svg/g, this.newHash);
               }
-            });
+            }).filter((n) => n);
           }
 
           // We have cached development source ._cachedSource... not in my house!
@@ -77,7 +80,12 @@ class WebpackSvgSpritely {
           const assetName = i.split('/')[i.split('/').length - 1].replace(/(.*)\.(.*)/, '$1');
           if (assetName === iconName) {
             if (!compilation.assets[i]._value) { return; }
-            let contents = compilation.assets[i]._value.toString('utf8');           
+            let contents = compilation.assets[i]._value.toString('utf8');
+            if (contents.indexOf('<style') !== -1) {
+              console.warn('\x1b[40m');
+              console.warn('\x1b[41m', `WARNING src/elements/atoms/Icons/assets/${assetName}.svg:\n<style> block found in this SVG file. Please relocate class based styles to inline attribute within svg file.`)
+              console.warn('\x1b[40m');
+            }
             contents = contents.replace(/<svg/g, '<symbol id="icon-'+this.icons[k].name+'"');
             contents = contents.replace(/<\/svg>/g, '</symbol>');
             contents = contents.replace('xmlns="http://www.w3.org/2000/svg"', '');
@@ -86,10 +94,10 @@ class WebpackSvgSpritely {
             this.symbols += contents;
           }
         });
-      });
+      }).filter((n) => n);
 
       compilation.assets[this.options.filename.replace(/iconset-(.*)\.svg/g, this.newHash)] = {
-        source: () => '<svg xmlns="http://www.w3.org/2000/svg" style="position:absolute; width: 0; height: 0">'+this.symbols+'</svg>',
+        source: () => `<svg xmlns="http://www.w3.org/2000/svg" style="position:absolute; width: 0; height: 0">${this.symbols}</svg>`,
         size: () => this.symbols.length
       };
     });
