c9b5aece244eb46d284a11e669390da9aec055af
drolsen <dolsen@connectivedx.com>
2020-06-29 04:00:51 -0700

    Adding babel polyfill for IE fixes

--- build/guide.jsx
+++ build/guide.jsx
@@ -4,6 +4,7 @@
   This is seperated from the ./build/assets.js to keep assets seperate guide.
   Both guide.js and guide.css are not intended to be use in production.
 */
+import '@babel/polyfill';
 import '@guide/guide.parts';
 import '@guide/styles'; // see ./build/guide/styles.css
 import '@guide/scripts'; // see ./guide/guide/scripts.jsx
