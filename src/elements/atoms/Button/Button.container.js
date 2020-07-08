7471abf38ace9169af47012664df3e5221da9a2b
drolsen <dolsen@connectivedx.com>
2020-07-08 09:14:15 -0700

    Adding string to boolean cheack prior to setAraState usage

--- src/elements/atoms/Button/Button.container.js
+++ src/elements/atoms/Button/Button.container.js
@@ -9,21 +9,19 @@ export const Button = (el) => {
 
   const setAriaState = () => {
     ariaState.pressed = !ariaState.pressed;
-    ui.el.setAttribute('aria-pressed', ariaState.pressed);
     ui.el.classList[ariaState.pressed ? 'add' : 'remove']('button--pressed');
+    ui.el.ariaPressed = ariaState.pressed;
   };
 
   // Buttons's main init method
   const init = () => {
-    if (ui.el.hasAttribute('aria-pressed')) {
-      ariaState.pressed = ui.el.getAttribute('aria-pressed');
+    if (ui.el.ariaPressed) {
+      ariaState.pressed = (ui.el.ariaPressed === 'true');
     }
 
     // check aria-pressed state (true, false, mixed, undefined)
-    if (ui.el.getAttribute('aria-pressed') === 'true' || ui.el.getAttribute('aria-pressed') === 'false') {
-      ui.el.addEventListener('click', () => {
-        setAriaState();
-      });
+    if (['true', 'false'].indexOf(ui.el.ariaPressed) !== -1) {
+      ui.el.addEventListener('click', () => setAriaState());
       ui.el.addEventListener('keydown', (e) => {
         if (e.which === 13 || e.which === 32) {
           e.preventDefault();
